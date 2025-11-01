7fe306880ee7c864cf16b8b9eda0496c9ccbbead
Muhammed Melih Budak <melih.budak@useinsider.com>
2025-11-01 19:39:47 +0300

    change hostname

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const main = async () => {
         })));
     })
 
-    server.listen(process.env.PORT ?? 3000, 'localhost', () => {
+    server.listen(process.env.PORT ?? 3000, () => {
         console.log(`Server running at http://localhost:${process.env.PORT ?? 3000}/`);
     });
 }
